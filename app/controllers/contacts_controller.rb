5bcaaece50c145bfaacc971c143c9837c6b15fdd
Faten Salmi <faten.salmi@gmail.com>
2019-12-03 16:58:42 +0100

    Tags create en cours

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,6 @@
 class ContactsController < ApplicationController
 
   def index
-    if params[:query].present?
-      sql_query = "first_name ILIKE :query OR last_name ILIKE :query"
-      @contacts = Contact.where(sql_query, query: "%#{params[:query]}%")
-    else
-      @contacts = Contact.all
-    end
   end
 
   def show
