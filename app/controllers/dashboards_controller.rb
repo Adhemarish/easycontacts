faf1fa902b656511b2632c5bb336a28f21c1bca4
Faten Salmi <faten.salmi@gmail.com>
2019-12-03 18:24:45 +0100

    searchbar en cours à continuer mercredi

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,7 @@ class DashboardsController < ApplicationController
         contacts.first_name @@ :query \
         OR contacts.last_name @@ :query \
         "
-      @contacts = Contact.where(sql_query, query: "%#{params[:query]}%")
+      @contacts = Contact.where(sql_query, query: params[:query])
     else
       @contacts = Contact.all
     end
@@ -14,4 +14,9 @@ end
 
 
 
+sql_query = []
 
+xxxx split " " each word
+  sql_query << truc first_name ILIKE '%word% OR last_name ILIKE same same'
+
+sql_query.join(" OR ")
