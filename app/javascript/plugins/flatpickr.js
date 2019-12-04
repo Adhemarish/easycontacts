cc76e5d29c571477079aaa871096d3ed716e53ba
Caroline Douet <caro.do@wanadoo.fr>
2019-12-04 17:28:21 +0100

    Calendar + date format

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,10 @@
+import flatpickr from "flatpickr"
+import { French } from "flatpickr/dist/l10n/fr.js"
+import "flatpickr/dist/themes/material_blue.css" // Note this is important!
+
+flatpickr(".datepicker", {
+  altInput: true,
+  altFormat: "l j F Y",
+  position: 'below',
+  locale: French,
+})
