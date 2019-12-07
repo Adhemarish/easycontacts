77314e22bfa84d7fb1eb5eb3c6a5593259405092
Ludovic Lhuillier <lhuillier.ludovic@yahoo.com>
2019-12-07 22:28:54 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   resources :tags, only: [:create, :destroy]
 
   resources :notes, only: [:edit, :update, :destroy]
+
+  resources :alerts, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/components', to: 'pages#components'
   get '/dashboard/results', to: 'dashboards#index'
