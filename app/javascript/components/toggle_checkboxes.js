435e2eaae72c58227ecd68e0c0dc218a0913d255
Neven DREAN <nevendrean@yahoo.fr>
2019-12-08 02:54:56 +0100

    debut search tag

--- app/javascript/components/toggle_checkboxes.js
+++ app/javascript/components/toggle_checkboxes.js
@@ -2,6 +2,7 @@ const checkboxList = document.getElementById('tagsCheckboxes')
 
 if (checkboxList) {
   checkboxList.addEventListener('change', (event) => {
+    event.preventDefault()
     event.target.parentNode.classList.toggle('blue-tag')
     }
   )
--- app/javascript/components/toggle_searchTags.js
+++ app/javascript/components/toggle_searchTags.js
@@ -1,8 +1,16 @@
-const tagsList = document.getElementById('search-tags')
+const tagsList = document.getElementById('tag-search')
+
 
 if (tagsList) {
   tagsList.addEventListener('change', (event) => {
+    event.preventDefault()
+    const searchBox = document.getElementById('query_query')
+    let aTag = event.target.parentNode.querySelector('.tag-text').innerHTML
+    console.log(aTag)
     event.target.parentNode.classList.toggle('blue-tag')
+    document.getElementById('target').appendChild(event.target.parentNode)
+    searchBox.value += aTag
+    searchBox.appendChild(event.target.parentNode)
     }
   )
 }
