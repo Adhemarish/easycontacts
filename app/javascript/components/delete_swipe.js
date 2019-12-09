75061f36fd296b07a95e23f3adee2da1fe836009
Ludovic Lhuillier <lhuillier.ludovic@yahoo.com>
2019-12-09 16:16:32 +0100

    contact trash

---
+++ app/javascript/components/delete_swipe.js
@@ -0,0 +1,11 @@
+const notes = document.querySelectorAll('.note')
+
+
+if (notes.length > 0) {
+  notes.forEach(note => {
+    if (swipedir =='left')
+        alert('You just swiped left!')
+  })
+}
+
+
