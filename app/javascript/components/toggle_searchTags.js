e2528da6237e31079ff96f7395aa5ba19450b98e
Neven DREAN <nevendrean@yahoo.fr>
2019-12-09 15:34:23 +0100

    fait search tag & add meeting place/date to contacts

--- app/javascript/components/toggle_searchTags.js
+++ app/javascript/components/toggle_searchTags.js
@@ -4,13 +4,15 @@ const tagsList = document.getElementById('tag-search')
 if (tagsList) {
   tagsList.addEventListener('change', (event) => {
     event.preventDefault()
-    event.target.parentNode.classList.toggle('blue-tag')
+    //event.target.parentNode.classList.toggle('blue-tag')
     const aTag = event.target.parentNode.querySelector('.tag-text').innerHTML
     const inputTag = document.getElementById('query_label')
-    inputTag.value += aTag
+    const tagNumber = parseInt(event.target.id.match(/\d+/))
+    console.log(tagNumber)
+    inputTag.value += aTag + " "
     inputTag.appendChild(event.target.parentNode)
     const searchBox = document.getElementById('target')
-    searchBox.value += aTag +" "
+    //searchBox.value += aTag +" "
     searchBox.appendChild(event.target.parentNode)
     }
   )
