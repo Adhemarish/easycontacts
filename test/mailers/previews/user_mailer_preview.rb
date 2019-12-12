28f4bb2bad0df210622e10bdd104ebbb6c12b37d
Ludovic Lhuillier <lhuillier.ludovic@yahoo.com>
2019-12-12 15:26:34 +0100

    postmark

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,8 +2,8 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
-  def welcome
-    UserMailer.welcome
+  def daily_alerts
+    UserMailer.with(user_id: 6).daily_alerts
   end
 
 end
